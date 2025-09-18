b3d937865061efc9f883b968f6e0a9c072a1e687
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2025-09-18 20:00:38 +0530

    Add files via upload

---
+++ day10_q20.c
@@ -0,0 +1,39 @@
+// Write a program to display the day of the week based on a number (1–7) using switch-case
+
+#include <stdio.h>
+
+int main(){
+
+int week;
+
+printf("Enter a number based on days of week:");
+scanf("%d",&week);
+switch (week)
+{
+case 1:
+  printf("Monday\n");
+  break;
+case 2:
+  printf("Tuesday\n");
+  break;
+case 3:
+  printf("Wednesday\n");
+  break;
+case 4:
+  printf("Thursday\n");
+  break;
+case 5:
+  printf("Friday\n");
+  break;
+case 6:
+  printf("Saturday\n");
+  break;
+case 7:
+  printf("Sunday\n");
+  break;      
+default:
+  printf("Error\n");        
+}
+
+return 0;
+}
